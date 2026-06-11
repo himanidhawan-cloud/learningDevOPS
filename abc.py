78d73b9db46cb32c381e6009eee0ec2572977dfa
himanidhawan <himanidhawan@LT-MBP-120.local>
2026-06-11 18:28:33 +0530

    ek din mai kuch kar lungi pata nhi sooo confusing

--- abc.py
+++ abc.py
@@ -2,3 +2,4 @@ print("hello world")
 print("i am just too dumb to understand what is going on in github may god bless me with the paitence to deal with it ")
 print("i summon the courage to deal with it and i hope that i will be able to understand it soon")
 print("zindagi bohot hi bekar hai mai pagal ho jaungi")
+password = 123455
