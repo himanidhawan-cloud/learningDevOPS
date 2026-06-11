d769efd9af2599ba93824121de65ecbd5558fe7b
himanidhawan <himanidhawan@LT-MBP-120.local>
2026-06-11 15:30:07 +0530

    you are the sunshine

---
+++ xyz.py
@@ -0,0 +1,6 @@
+print("hello world")
+print("hello world")
+print("hello world")                
+i am just too dumb to understand what is going on in github may god 
+bless me with the paitence to deal with it 
+i summon the courage to deal with it and i hope that i will be able to understand it soon
