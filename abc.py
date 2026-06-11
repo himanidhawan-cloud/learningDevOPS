532b6753b2651cde4eab5989bba762bfe0519c19
himanidhawan <himanidhawan@LT-MBP-120.local>
2026-06-11 15:28:08 +0530

    please

--- abc.py
+++ abc.py
@@ -1,2 +1,3 @@
 print("hello world")
-
+print("i am just too dumb to understand what is going on in github may god bless me with the paitence to deal with it ")
+print("i summon the courage to deal with it and i hope that i will be able to understand it soon")
